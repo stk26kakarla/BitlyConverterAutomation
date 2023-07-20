55140a124145f38844356353f643d7547cbbeb01
stk26kakarla <31064675+stk26kakarla@users.noreply.github.com>
2023-07-20 21:01:41 +0530

    Add files via upload

---
+++ BitlyConverterAutomation/BitlyConverter.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace BitlyConverterAutomation
+{
+    partial class BitlyConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BitlyConverter));
+            this.LongURLsTextbox = new System.Windows.Forms.TextBox();
+            this.ConvertToBitly = new System.Windows.Forms.Button();
+            this.ResponseProgress = new System.Windows.Forms.ProgressBar();
+            this.ShortenedURLsTextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.QRCodesTextbox = new System.Windows.Forms.TextBox();
+            this.QRCodeFilePathTextbox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SecondHalfNamesTextbox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LongURLsTextbox
+            // 
+            this.LongURLsTextbox.Location = new System.Drawing.Point(12, 47);
+            this.LongURLsTextbox.MaxLength = 0;
+            this.LongURLsTextbox.Multiline = true;
+            this.LongURLsTextbox.Name = "LongURLsTextbox";
+            this.LongURLsTextbox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.LongURLsTextbox.Size = new System.Drawing.Size(334, 629);
+            this.LongURLsTextbox.TabIndex = 0;
+            // 
+            // ConvertToBitly
+            // 
+            this.ConvertToBitly.Location = new System.Drawing.Point(639, 306);
+            this.ConvertToBitly.Name = "ConvertToBitly";
+            this.ConvertToBitly.Size = new System.Drawing.Size(163, 34);
+            this.ConvertToBitly.TabIndex = 2;
+            this.ConvertToBitly.Text = "Convert";
+            this.ConvertToBitly.UseVisualStyleBackColor = true;
+            this.ConvertToBitly.Click += new System.EventHandler(this.ConvertToBitly_Click);
+            // 
+            // ResponseProgress
+            // 
+            this.ResponseProgress.Location = new System.Drawing.Point(12, 699);
+            this.ResponseProgress.Name = "ResponseProgress";
+            this.ResponseProgress.Size = new System.Drawing.Size(1323, 22);
+            this.ResponseProgress.TabIndex = 3;
+            this.ResponseProgress.Visible = false;
+            // 
+            // ShortenedURLsTextbox
+            // 
+            this.ShortenedURLsTextbox.Location = new System.Drawing.Point(824, 46);
+            this.ShortenedURLsTextbox.MaxLength = 0;
+            this.ShortenedURLsTextbox.Multiline = true;
+            this.ShortenedURLsTextbox.Name = "ShortenedURLsTextbox";
+            this.ShortenedURLsTextbox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.ShortenedURLsTextbox.Size = new System.Drawing.Size(263, 629);
+            this.ShortenedURLsTextbox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 29);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Long URLs:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(832, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 29);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Shortened URLs:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1130, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 29);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "QR Codes:";
+            // 
+            // QRCodesTextbox
+            // 
+            this.QRCodesTextbox.Location = new System.Drawing.Point(1135, 46);
+            this.QRCodesTextbox.MaxLength = 0;
+            this.QRCodesTextbox.Multiline = true;
+            this.QRCodesTextbox.Name = "QRCodesTextbox";
+            this.QRCodesTextbox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.QRCodesTextbox.Size = new System.Drawing.Size(200, 630);
+            this.QRCodesTextbox.TabIndex = 4;
+            // 
+            // QRCodeFilePathTextbox
+            // 
+            this.QRCodeFilePathTextbox.Location = new System.Drawing.Point(639, 97);
+            this.QRCodeFilePathTextbox.Multiline = true;
+            this.QRCodeFilePathTextbox.Name = "QRCodeFilePathTextbox";
+            this.QRCodeFilePathTextbox.Size = new System.Drawing.Size(163, 170);
+            this.QRCodeFilePathTextbox.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(646, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "QRCode File path:";
+            // 
+            // SecondHalfNamesTextbox
+            // 
+            this.SecondHalfNamesTextbox.Location = new System.Drawing.Point(368, 46);
+            this.SecondHalfNamesTextbox.MaxLength = 0;
+            this.SecondHalfNamesTextbox.Multiline = true;
+            this.SecondHalfNamesTextbox.Name = "SecondHalfNamesTextbox";
+            this.SecondHalfNamesTextbox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.SecondHalfNamesTextbox.Size = new System.Drawing.Size(253, 629);
+            this.SecondHalfNamesTextbox.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(363, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(246, 29);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Second Half Names:";
+            // 
+            // BitlyConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1347, 733);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.SecondHalfNamesTextbox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.QRCodeFilePathTextbox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.QRCodesTextbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ShortenedURLsTextbox);
+            this.Controls.Add(this.ResponseProgress);
+            this.Controls.Add(this.ConvertToBitly);
+            this.Controls.Add(this.LongURLsTextbox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(970, 570);
+            this.Name = "BitlyConverter";
+            this.Text = "OneShell URL Shortner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LongURLsTextbox;
+        private System.Windows.Forms.Button ConvertToBitly;
+        private System.Windows.Forms.ProgressBar ResponseProgress;
+        private System.Windows.Forms.TextBox ShortenedURLsTextbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox QRCodesTextbox;
+        private System.Windows.Forms.TextBox QRCodeFilePathTextbox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox SecondHalfNamesTextbox;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ BitlyConverterAutomation/BitlyConverter.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft;
+using System.Web;
+using Newtonsoft.Json;
+using RestSharp;
+using System.IO;
+
+namespace BitlyConverterAutomation
+{
+    public partial class BitlyConverter : Form
+    {
+        public BitlyConverter()
+        {
+            InitializeComponent();
+        }
+
+        public class BulkAction
+        {
+            public virtual string LongURL { get; set; }
+            public virtual string SecondHalfName { get; set; }
+        }
+
+        private void ConvertToBitly_Click(object sender, EventArgs e)
+        {
+            ShortenedURLsTextbox.Text = "";
+            QRCodesTextbox.Text = "";
+            IList<string> LongURLsList = LongURLsTextbox.Text.Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            IList<string> SecondHalfNamesList = SecondHalfNamesTextbox.Text.Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            //!string.IsNullOrEmpty(LongURLsText) ? LongURLsText.Cast<object>().Select(o => o.ToString()).ToList() : new List<string>();
+
+            IList<BulkAction> BulkActions = new List<BulkAction>();
+
+            for (int i = 0; i < LongURLsList.Count; i++)
+            {
+                BulkActions.Add(new BulkAction
+                {
+                    LongURL = LongURLsList[i],
+                    SecondHalfName = i < SecondHalfNamesList.Count ? SecondHalfNamesList[i] : ""
+                });
+            }
+
+            ResponseProgress.Visible = true;
+            ResponseProgress.Maximum = BulkActions.Count;
+            ResponseProgress.Step = 1;
+            int listItem = 1;
+            string LeeAccessToken = "059d1559e67ba1526e8c99116d3fd5a5572ca3cd";
+            string BitlyApi = "https://api-ssl.bitly.com/v4/";
+            var client = new RestClient(BitlyApi);
+            var request = new RestRequest(Method.POST);
+            IRestResponse response;
+
+            foreach (BulkAction BulkAction in BulkActions)
+            {
+                try
+                {
+                    ResponseObject ResponseObject;
+                    #region Shortening URLs
+                    BitlyApi = "https://api-ssl.bitly.com/v4/shorten";
+
+                    client = new RestClient(BitlyApi);
+                    request = new RestRequest(Method.POST);
+                    request.AddHeader("Authorization", "Bearer " + LeeAccessToken);
+                    request.AddHeader("Content-Type", "application/json");
+                    request.AddJsonBody(JsonConvert.SerializeObject(
+                        new
+                        {
+                            long_url = BulkAction.LongURL,
+                            domain = "go.shell.com"
+                        }));
+                    response = client.Execute(request);
+                    ResponseObject = JsonConvert.DeserializeObject<ResponseObject>(response.Content);
+                    #endregion
+
+                    if (!string.IsNullOrEmpty(BulkAction.SecondHalfName))
+                    {
+                        #region Custom Bitlinks
+                        BitlyApi = "https://api-ssl.bitly.com/v4/custom_bitlinks";
+
+                        client = new RestClient(BitlyApi);
+                        request = new RestRequest(Method.POST);
+                        request.AddHeader("Authorization", "Bearer " + LeeAccessToken);
+                        request.AddHeader("Content-Type", "application/json");
+                        request.AddJsonBody(JsonConvert.SerializeObject(
+                            new
+                            {
+                                custom_bitlink = "go.shell.com/" + BulkAction.SecondHalfName,
+                                bitlink_id = ResponseObject.id//link.Replace("https://", "")
+                            }));
+                        response = client.Execute(request);
+                        ResponseObject = JsonConvert.DeserializeObject<ResponseObject>(response.Content);
+                        PrintResponse(
+                            ShortenedURLsTextbox,
+                            ResponseObject.custom_bitlink != null ? "https://" + ResponseObject.custom_bitlink : ResponseObject.description,
+                            ResponseObject.custom_bitlink != null ? true : false);
+                        #endregion
+                    }
+                    else
+                    {
+                        PrintResponse(
+                        ShortenedURLsTextbox,
+                        ResponseObject.id != null ? ResponseObject.link : ResponseObject.description,
+                        ResponseObject.id != null ? true : false);
+                    }
+
+                    #region QR Code
+                    BitlyApi = "https://api-ssl.bitly.com/v4/bitlinks/" + ResponseObject.id + "/qr";
+                    client = new RestClient(BitlyApi);
+                    request = new RestRequest(Method.GET);
+                    request.AddHeader("Authorization", "Bearer " + LeeAccessToken);
+                    request.AddHeader("Content-Type", "application/json");
+
+                    response = client.Execute(request);
+                    ResponseObject = JsonConvert.DeserializeObject<ResponseObject>(response.Content);
+
+                    PrintResponse(
+                        QRCodesTextbox,
+                        ResponseObject.id != null ? ResponseObject.qr_code : ResponseObject.description,
+                        ResponseObject.id != null ? true : false);
+
+                    string QRCodeSavePath = Path.Combine(QRCodeFilePathTextbox.Text, listItem + ".png");
+                    if (File.Exists(QRCodeSavePath))
+                    {
+                        File.Delete(QRCodeSavePath);
+                    }
+                    File.WriteAllBytes(QRCodeSavePath,
+                        Convert.FromBase64String(ResponseObject.qr_code.Split(',')[1]));
+
+                    #endregion
+
+                    ResponseProgress.Value = listItem++;
+                }
+                catch (Exception ex)
+                {
+                    PrintResponse(QRCodesTextbox, "Exception Caught!\nMessage : " + ex.Message, false);
+                }
+            }
+
+            //ResponseProgress.Visible = false;
+        }
+
+
+        private void PrintResponse(TextBox Response, string Message, bool IsSuccess)
+        {
+            if (IsSuccess)
+            {
+                Response.Font = new Font(FontFamily.GenericSansSerif, 10, FontStyle.Regular);
+
+            }
+            else
+            {
+                Response.ForeColor = Color.Red;
+                Response.Font = new Font(FontFamily.GenericSansSerif, 10, FontStyle.Bold);
+            }
+
+            Response.Visible = true;
+            Response.Text += Message + "\r\n";
+        }
+    }
+
+    public class ResponseObject
+    {
+        private string _bitlinkId;
+        public virtual string custom_bitlink { get; set; }
+        public bitlink bitlink { get; set; }
+        public virtual string id
+        {
+            get { return _bitlinkId ?? bitlink.id; }
+            set { _bitlinkId = value; }
+        }
+        public virtual string link { get; set; }
+        public virtual string qr_code { get; set; }
+        public virtual string message { get; set; }
+        public virtual string description { get; set; }
+        public virtual string resource { get; set; }
+        public virtual Error[] errors { get; set; }
+        public virtual object noData { get; set; }
+
+    }
+
+    public class bitlink
+    {
+        public virtual string id { get; set; }
+    }
+
+    public class Error
+    {
+        public virtual string field { get; set; }
+        public virtual string error_code { get; set; }
+        public virtual string message { get; set; }
+    }
+}
